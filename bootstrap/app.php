b4dfcf08d36c1e7af4fd2747d7e38974a5ec29aa
prajarp <rajkowskipiotr23@gmail.com>
2024-12-07 14:19:34 +0100

    Added php stan. changed getCoordinatesWithOrders

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,14 +5,27 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    // Rejestrowanie tras
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+
+    // Rejestrowanie middleware
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Tutaj dodaj middleware, jeśli jest wymagany
     })
+
+    // Rejestrowanie dostawców usług
+    ->withProviders([
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        // Dodaj innych dostawców usług, jeśli potrzebujesz
+    ])
+
+    // Rejestrowanie wyjątków
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        // Tutaj możesz obsłużyć specyficzne wyjątki
+    })
+
+    ->create();
